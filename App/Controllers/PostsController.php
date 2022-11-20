0e26f60152751dcceafe65bacf0a3e0585436a70
Tafara Shamu <talph@outlook.com>
2022-11-20 09:58:34 +0200

    first commit

---
+++ App/Controllers/PostsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers;
+
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        echo 'Hello world';
+    }
+
+    public function show($id)
+    {
+        // TODO: Implement show() method.
+    }
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function store(...$arguments)
+    {
+        // TODO: Implement store() method.
+    }
+
+    public function edit($id)
+    {
+        // TODO: Implement edit() method.
+    }
+
+    public function softDelete($id)
+    {
+        // TODO: Implement softDelete() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use Core\Router;
+
+spl_autoload_register(function ($class) {
+    $root = dirname(__DIR__);
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+    if (is_readable($file)) {
+        require $file;
+    }
+});
+
+/*
+ * Routing
+ */
+
+$router = new Router();
+
+$router->add('', ['controller' => 'HomeController', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+
+$router->dispatch($_SERVER['QUERY_STRING']);
