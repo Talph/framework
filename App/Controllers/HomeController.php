49487a70182e81c77560274a3bf852002e639898
Tafara Shamu <talph@outlook.com>
2022-11-20 10:08:03 +0200

    Add controler abstract class in core

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 
+use Core\Controller;
+
 class HomeController extends Controller
 {
     public function index()
--- App/Controllers/PostsController.php
+++ App/Controllers/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 
+use Core\Controller;
+
 class PostsController extends Controller
 {
     public function index()
